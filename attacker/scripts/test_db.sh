6bc9f78d9e969ddc4736c8358a6dfb48ee8f0933
Izzac <izzac@izzacs-laboratory.tailad30fd.ts.net>
2025-08-29 12:25:44 -0400

    workstation bug

--- attacker/scripts/test_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "[TEST] Testing captured credentials on database..."
-mysql -h 172.20.0.10 -u sarah_chen -p'Portfolio2024!' customer_db --ssl-mode=DISABLED -e "SELECT * FROM clients;"
